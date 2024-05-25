f6b011b1a95cbf2f3f6b7a68677306fd6491cbd3
Igor Becker <igor_nyu@hotmail.com>
2024-05-24 21:18:46 -0300

    feat: collision with player and enemy

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <time.h>
 
 #include <raylib.h>
+#include <raymath.h>
 
 #define WIDTH       800
 #define HEIGHT      600
@@ -23,14 +24,19 @@ typedef struct {
 
 typedef struct {
     unsigned short int size;
-    Vector2 direction;
     Vector2 position;
+    Vector2 velocity;
 } Ball;
 
-static Player *player = NULL;
-static Player *enemy = NULL;
-static Ball *ball = NULL;
-bool isGamePaused = false;
+typedef struct {
+    Player player;
+    Player enemy;
+    Ball ball;
+    bool running;
+    float deltaTime;
+} State;
+
+static State *state = NULL;
 
 void init_character(Player *p, float posX, Color color) {
     int halfScreen = GetScreenHeight() / 2 - P_HEIGHT/2;
@@ -41,17 +47,18 @@ void init_character(Player *p, float posX, Color color) {
     p->c = color;
 }
 
-void draw(Player *p) {
-    DrawRectangle(p->position.x, p->position.y, p->w, p->h, p->c);
+void init_ball() {
+    float x = (float)GetScreenWidth() / 2;
+    float y = (float)GetScreenHeight() / 2;
+    state->ball.position.x = x;
+    state->ball.position.y = y;
+    state->ball.size = 8;
+    state->ball.velocity.x = SPEED;
+    state->ball.velocity.y = SPEED;
 }
 
-void player_movement(float dt) {
-    if (IsKeyDown(KEY_W) && player->position.y > 0) {
-        player->position.y -= SPEED * dt;
-    }
-    if (IsKeyDown(KEY_S) && player->position.y < HEIGHT - player->h) {
-        player->position.y += SPEED * dt;
-    }
+void draw_player(Player *p) {
+    DrawRectangle(p->position.x, p->position.y, p->w, p->h, p->c);
 }
 
 void draw_middle_divisor() {
@@ -64,78 +71,24 @@ void draw_middle_divisor() {
             );
 }
 
-void score() {
-    DrawText("Score: ", 40, 10, 16, WHITE);
-}
-
-void init_ball() {
-    int halfScreenX = GetScreenWidth() / 2;
-    int halfScreenY = GetScreenHeight() / 2;
-    ball->direction.x = -1;
-    ball->direction.y = 0.3f;
-    ball->size = 8.0f;
-    ball->position.x = (float)halfScreenX;
-    ball->position.y = (float)halfScreenY;
-}
-
 void draw_ball() {
-    DrawCircle(ball->position.x, ball->position.y, ball->size, YELLOW);
+    DrawCircleV(state->ball.position, state->ball.size, WHITE);
 }
 
-bool is_colliding(float dt) {
-    bool collidingWithPlayer = 
-        ball->position.x - (int)ball->size - (SPEED * dt) <= player->position.x + player->w &&
-        ball->position.y - (int)ball->size >= player->position.y &&
-        ball->position.y + (int)ball->size <= player->position.y + player->h;
-
-    bool collidingWithEnemy = 
-        ball->position.x + (int)ball->size + (SPEED * dt) >= enemy->position.x &&
-        ball->position.y + (int)ball->size >= enemy->position.y &&
-        ball->position.y + (int)ball->size <= enemy->position.y + enemy->h;
-
-    if (collidingWithPlayer || collidingWithEnemy) return true;
-    return false;
-}
-
-void update_ball(float dt) {
-    ball->position.x += ball->direction.x * (SPEED * dt);
-
-    bool isCollinding = is_colliding(dt);
-    if (isCollinding) {
-        int middle_player_point_y = (player->position.y + player->h) / 2;
-
-        ball->direction.x = (float)((int)ball->direction.x * -1);
-    }
-}
-
-void clear() {
-    free(player);
-    free(enemy);
-    free(ball);
-}
-
-void keyboard_events() {
-    if (IsKeyPressed(KEY_SPACE)) {
-        isGamePaused = !isGamePaused;
-    }
+void init_state() {
+    init_character(&state->player, 5, RED);
+    init_character(&state->enemy, GetScreenWidth() - P_WIDTH - 5, GREEN);
+    init_ball();
 }
 
 void init() {
-    player = malloc(sizeof(*player));
-    assert(player != NULL);
-    memset(player, 0, sizeof(*player));
+    state = malloc(sizeof(*state));
+    assert(state != NULL);
+    memset(state, 0, sizeof(*state));
 
-    enemy = malloc(sizeof(*enemy));
-    assert(enemy != NULL);
-    memset(enemy, 0, sizeof(*enemy));
+    state->running = true;
 
-    ball = malloc(sizeof(*ball));
-    assert(ball != NULL);
-    memset(ball, 0, sizeof(*ball));
-
-    init_character(player, 5, RED);
-    init_character(enemy, GetScreenWidth() - P_WIDTH - 5, GREEN);
-    init_ball();
+    init_state();
 }
 
 void draw_pause_text() {
@@ -143,19 +96,72 @@ void draw_pause_text() {
     DrawText("Paused!", GetScreenWidth() / 2 - (fontSize * 2), GetScreenHeight() / 2 - fontSize, fontSize, WHITE);
 }
 
-void update(float dt) {
-    score();
+void keyboard_events() {
+    if (IsKeyPressed(KEY_SPACE)) {
+        state->running = !state->running;
+    }
+    if (IsKeyPressed(KEY_R)) {
+        init_state();
+    }
 
-    draw(player);
-    draw(enemy);
+    if (state->running) {
+        // player movement
+        if (IsKeyDown(KEY_W) && state->player.position.y > 0) {
+            state->player.position.y -= SPEED * state->deltaTime;
+        }
+        if (IsKeyDown(KEY_S) && state->player.position.y < HEIGHT - state->player.h) {
+            state->player.position.y += SPEED * state->deltaTime;
+        }
+
+        // enemy movement
+        if (IsKeyDown(KEY_UP) && state->player.position.y > 0) {
+            state->enemy.position.y -= SPEED * state->deltaTime;
+        }
+        if (IsKeyDown(KEY_DOWN) && state->enemy.position.y < HEIGHT - state->enemy.h) {
+            state->enemy.position.y += SPEED * state->deltaTime;
+        }
+    }
+}
+
+bool ball_collision_wall() {
+    return state->ball.position.y + state->ball.size >= HEIGHT || state->ball.position.y - state->ball.size <= 0;
+}
+
+bool ball_collision_player() {
+    return state->ball.position.x - state->ball.size <= state->player.position.x + state->player.w &&
+        state->ball.position.y >= state->player.position.y &&
+        state->ball.position.y <= state->player.position.y + state->player.h;
+}
+
+bool ball_collision_enemy() {
+    return state->ball.position.x + state->ball.size >= state->enemy.position.x &&
+        state->ball.position.y >= state->enemy.position.y &&
+        state->ball.position.y <= state->enemy.position.y + state->enemy.h;
+}
+
+void update_ball() {
+    state->ball.position.x += (-state->ball.velocity.x) * state->deltaTime;
+    state->ball.position.y += (state->ball.velocity.y) * state->deltaTime;
+
+    if (ball_collision_wall()) {
+        state->ball.velocity.y = -state->ball.velocity.y;
+    }
+
+    if (ball_collision_player() || ball_collision_enemy()) {
+        state->ball.velocity.x = -state->ball.velocity.x;
+    }
+}
+
+void update() {
+    draw_player(&state->player);
+    draw_player(&state->enemy);
     draw_ball();
     draw_middle_divisor();
 
-    if(!isGamePaused) {
-        update_ball(dt);
-        player_movement(dt);
-    } else {
+    if(!state->running) {
         draw_pause_text();
+    } else {
+        update_ball();
     }
 }
 
@@ -166,14 +172,14 @@ int main(void) {
     init();
 
     while (!WindowShouldClose()) {
-        float dt = GetFrameTime();
+        state->deltaTime = GetFrameTime();
         BeginDrawing();
             ClearBackground(BLACK);
             keyboard_events();
-            update(dt);
+            update();
         EndDrawing();
     }
-    clear();
+    free(state);
     CloseWindow();
     return 0;
 }
